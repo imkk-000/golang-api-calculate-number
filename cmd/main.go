eafe3abfffe683899c8fd1109482fe430eed9c9b
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-03-15 16:21:16 +0700

    [Edited]: http.HandleFunc to http.Handle in cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -14,12 +14,12 @@ func main() {
 	var port uint
 	flag.UintVar(&port, "port", 8888, "Set port for api listening")
 
-	myAPI := api.API{
+	calculateAPI := api.CalculateAPI{
 		Read:      ioutil.ReadAll,
 		Marshal:   json.Marshal,
 		Unmarshal: json.Unmarshal,
 	}
-	http.HandleFunc("/calculate", myAPI.CalculateAPI)
+	http.Handle("/calculate", calculateAPI)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	log.Fatal(err)
 }
